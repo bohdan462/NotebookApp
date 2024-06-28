b47abc0e03297d36c493b95cd5d44dca8ad33392
Bohdan Tkachenko <techbohdan@gmail.com>
2024-06-28 14:44:29 -0500

    Initial Commit

---
+++ Notebook/NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotebookApp.swift
+//  Notebook
+//
+//  Created by Bohdan Tkachenko on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
